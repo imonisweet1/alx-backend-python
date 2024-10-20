d500d9a61196a68baf5e28e78111e3154ee535b1
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-10-20 16:45:52 +0000

    made more changes

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
-"""
-Annotation Function for 'add'
-"""
+"""Type-annotated function add"""
 
 
 def add(a: float, b: float) -> float:
-    """
-    Adds two floats.
-
-    Args:
-        a (float): first float
-        b (float): second float
-
-    Returns:
-        float: sum of the arguments
-    """
+    """Takes two floats and returns their sum as float"""
     return a + b
